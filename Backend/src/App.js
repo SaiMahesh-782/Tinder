32f358b62722eab4b3b0ca0529a242b9e45ea451
SaiMahesh-782 <saimahesh645@gmail.com>
2025-06-17 23:38:11 +0530

    second commit

--- Backend/src/App.js
+++ Backend/src/App.js
@@ -1,1 +1,22 @@
-console.log("stating")
+const express = require('express');
+const app = express();
+
+// Route for root
+app.get("/", (req, res) => {
+    res.send("Hello from server");
+});
+
+// Route for /Browse
+app.get("/Browse", (req, res) => {
+    res.send("Browse from server");
+});
+
+// Route for /test
+app.get("/test", (req, res) => {
+    res.send("test from server");
+});
+
+// Start server
+app.listen(3000, () => {
+    console.log("Server is started on port 3000");
+});
