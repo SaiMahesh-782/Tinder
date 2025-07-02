4c4f2a598ec01d288e2d5e3dfbe42f563f61b4bd
SaiMahesh-782 <saimahesh645@gmail.com>
2025-07-02 07:05:16 +0530

    findsingleiuser

--- Backend/src/App.js
+++ Backend/src/App.js
@@ -1,30 +1,35 @@
 const express = require('express');
 const app = express();
 const connectdb = require('../config/database');
-const UserModel=require('../models/user')
+const UserModel = require('../models/user');
 
+app.use(express.json());
 
+// POST: Signup
+app.post('/signup', async (req, res) => {
+  const user = new UserModel(req.body);
 
-app.post('/signup',async (req,res)=>{
-  const user= new UserModel({
-    firstName :"virat",
-    lastName:"mahesh",
-    emailId:"saimahesh@gmai.com"
-
-  })
-
-  try{
-     await user.save()
-  res.send("user added")
+  try {
+    await user.save();
+    res.status(201).send("User added successfully");
+  } catch (err) {
+    res.status(400).send({ error: err.message });
   }
- catch(err){
-  res.status(401).send("error message",+ err.message)
- }
+});
 
+// GET: Fetch user by email (using query param)
+app.get('/feeduser', async (req, res) => {
+  try {
+    const userEmail = req.body.emailId// e.g., /feeduser?emailId=test@example.com
+    const user = await UserModel.findOne({ emailId: userEmail });
+    res.status(200).send(user);
+  } catch (err) {
+    console.error("Something went wrong:", err.message);
+    res.status(500).send({ error: "Internal Server Error" });
+  }
+});
 
-})
-
-
+// Connect DB and Start Server
 connectdb()
   .then(() => {
     console.log("Connected to database");
